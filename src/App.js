4060702f21ef2f390e4cb3f58f64376dd9fb579a
Lav <lovepanchal1912@gmail.com>
2021-07-06 20:32:49 +0530

    Done shop page and its components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Homepage } from "./pages/homepage/Homepage";
+import Shop from "./pages/shop/Shop";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
+      </Switch>
     </>
   );
 }
