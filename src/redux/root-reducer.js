d17ae992670e22c052b80585a7b5946a3d41c14c
Lav <lovepanchal1912@gmail.com>
2021-07-21 01:21:41 +0530

    added redux-persist library and store cart data into local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,17 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import CartReducer from "./cart/cart.reducer";
-export default combineReducers({
+const persistconfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: CartReducer,
 });
+
+export default persistReducer(persistconfig, rootReducer);
