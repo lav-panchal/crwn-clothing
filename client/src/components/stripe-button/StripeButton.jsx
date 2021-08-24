9a2ed034c63101bb40f6715865ad897cc989d726
Lav <lovepanchal1912@gmail.com>
2021-08-24 13:45:33 +0530

    separating client folder and make express server for stripe payment

--- client/src/components/stripe-button/StripeButton.jsx
+++ client/src/components/stripe-button/StripeButton.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishablekey =
     "pk_test_51JHA3ISHAS2IzWlQywTQdISluOQTZqytaxNcvboBUlj9v3plpzEbBqEzzqpbCKE3D4zx3nLLnnQ4WA7sH6QnAZYK00qrRKRFsQ";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment. Please sure you use the provided credit card."
+        );
+      });
   };
   return (
     <StripeCheckout
